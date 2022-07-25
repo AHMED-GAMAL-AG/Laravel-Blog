6b9a56c2835803903d83dea601508e63e41b33d5
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-07-25 14:48:35 +0200

    Use Caching for Expensive Operations -  lec 10

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -21,15 +21,17 @@ Route::get(
     "/posts/{slug_name}",
     function ($slug) {
 
-        $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-        if (!file_exists($path)) {
+        if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
             return redirect("/"); //return home page
             // abort(404);
         }
 
-        $post =  file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+        $post = cache()->remember("post.{slug_name}", 1200, fn () => file_get_contents($path)); // cache for 20 minutes 1200 sec
+
+        //$post =  file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+
         return view("post", [
             "post" => $post
         ]);
-    })->where("slug_name", "[A-z_\-]+"); // regex for slug name capital and small letters, underscore and dash + is for one or more characters
+    }
+)->where("slug_name", "[A-z_\-]+"); // regex for slug name capital and small letters, underscore and dash + is for one or more characters
