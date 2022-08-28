cc09123f8809504afd83a2aa5b0378693454d54f
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-08-28 14:26:42 +0200

    Write the Markup for a Post Comment - 52

---
+++ blog/resources/views/components/post-comment.blade.php
@@ -0,0 +1,24 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60?img=67" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Ahmed Gamal</h3>
+
+            <p class="text-xs">
+                Posted
+                <time>8 days ago</time>
+            </p>
+
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+        </p>
+    </div>
+
+</article>
