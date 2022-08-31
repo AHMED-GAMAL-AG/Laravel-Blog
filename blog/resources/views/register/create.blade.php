abc4e3aa19109215706119961b93c01fc3faf141
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-08-31 23:13:35 +0200

    added select gender in registration

--- blog/resources/views/register/create.blade.php
+++ blog/resources/views/register/create.blade.php
@@ -60,6 +60,39 @@
                 </div>
 
 
+                <div>
+                    <div class="mb-2">
+                        <label class="block uppercase font-bold text-xs text-gray-700" for="gender">
+                            Select your gender
+                        </label>
+                    </div>
+
+                    <div class=" relative flex lg:inline-flex items-center bg-gray-200 rounded-xl mb-6" name="Gender">
+
+
+                        <select class=" flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                            <option value="category" disabled selected>Gender
+                            </option>
+                            <option value="male">Male
+                            </option>
+                            <option value="female">Female
+                            </option>
+                        </select>
+
+                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;"
+                            width="22" height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                                </path>
+                                <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                                </path>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+
+
                 <div class="mb-6">
                     <button type="submit"
                         class="bg-blue-400 text-white rounded p-2 px-4 hover:bg-blue-500">submit</button>
