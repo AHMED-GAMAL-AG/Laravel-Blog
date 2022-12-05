17983df5fae19649eea2ac80045cd2ad382edc0e
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-05 14:11:00 +0200

    Group and Store Validation Logic - 68

--- blog/app/Http/Controllers/AdminPostController.php
+++ blog/app/Http/Controllers/AdminPostController.php
@@ -25,20 +25,16 @@ class AdminPostController extends Controller
 
     public function store()
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'required|image',
-            'slug' => ['required', ValidationRule::unique('posts', 'slug')], // this has to be unique in the posts table on the slug column
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', ValidationRule::exists('categories', 'id')] // make sure that this id exists in the categories table
-        ]);
+        // $attributes = $this->validatePost(); // i extract the validation to a function to make it reusable
+        // $attributes['user_id'] = auth()->id(); // add this attribute to the attributes array after validation
+        // $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails'); // store the file in the thumbnails folder and return the path to the file and the storage/public is linked to the public folder by using the php artisan storage:link command
+        // Post::create($attributes);
 
-        // add this attribute to the attributes array after validation
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails'); // store the file in the thumbnails folder and return the path to the file and the storage/public is linked to the public folder by using the php artisan storage:link command
-
-        Post::create($attributes);
+        // same as above
+        Post::create(array_merge($this->validatePost(), [
+            'user_id' => auth()->id(),
+            'thumbnail' => request()->file('thumbnail')->store('thumbnails')
+        ]));
 
         return redirect('/');
     }
@@ -52,15 +48,9 @@ class AdminPostController extends Controller
 
     public function update(Post $post)
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'image',
-            'slug' => ['required', ValidationRule::unique('posts', 'slug')->ignore($post->id)], // this is already a post in the database so ignore it
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', ValidationRule::exists('categories', 'id')] // make sure that this id exists in the categories table
-        ]);
+        $attributes = $this->validatePost(); // i extract the validation to a function to make it reusable
 
+        // same as if($attributes['thumbnail'] ?? false) // means check for a thumbnail but asume we dont have one
         if(isset($attributes['thumbnail'])) {
             $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails'); // store the file in the thumbnails folder and return the path to the file and the storage/public is linked to the public folder by using the php artisan storage:link command
         }
@@ -75,5 +65,19 @@ class AdminPostController extends Controller
         return back()->with('success', 'Post Deleted!');
     }
 
+
+    protected function validatepost(?Post $post = null): array // null can be passed to this method you dont have to give it a post if you dont want to
+    {
+        $post = $post ?? new Post(); // if you gave a post it will use it other wise it will use that new post
+        return request()->validate([
+            'title' => 'required',
+            'thumbnail' => $post->exists ? ['image'] : ['required','image'], // if the post exists then the thumbnail is optional but if it doesnt exist then it is required
+            'slug' => ['required', ValidationRule::unique('posts', 'slug')->ignore($post)], //this has to be unique in the posts table on the slug column for when iam creating a new post the ->ignore($post) part iam creating a new post thier is no id aim not ignoring any thing BUT if this is already a post in the database ignore it $post same as $post->id it automatically gets the id
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', ValidationRule::exists('categories', 'id')], // make sure that this id exists in the categories table
+            'puplished_at' => 'required',
+        ]);
+    }
 
 }
